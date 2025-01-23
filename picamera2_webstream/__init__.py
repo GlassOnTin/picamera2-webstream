fb7ed4a02fa2af37df12c00420bf5908a306f405
GlassOnTin <63980135+GlassOnTin@users.noreply.github.com>
2025-01-23 17:57:06 +0000

    Bump version to 0.2.7: bump version

--- picamera2_webstream/__init__.py
+++ picamera2_webstream/__init__.py
@@ -1,5 +1,5 @@
 from .stream_picamera import VideoStream, create_app
 #from .stream_ffmpeg import VideoStream, create_app as create_ffmpeg_app
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 __all__ = ['VideoStream', 'create_app']
